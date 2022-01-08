524e6fadb387bc65b235d1408cda9982c77e34fc
MJbae <bmj5208@gmail.com>
2022-01-08 11:36:52 +0900

    feat: add post model

--- sns/models.py
+++ sns/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 
 
@@ -7,4 +8,23 @@ class TimestampedModel(models.Model):
 
     class Meta:
         abstract = True
+
+
+class Post(TimestampedModel):
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL, related_name="my_post_set", on_delete=models.CASCADE
+    )
+    photo = models.ImageField(upload_to="instagram/post/%Y/%m/%d")
+    caption = models.CharField(max_length=500)
+    tag_set = models.ManyToManyField("Tag", blank=True)
+    location = models.CharField(max_length=100)
+    like_user_set = models.ManyToManyField(
+        settings.AUTH_USER_MODEL, blank=True, related_name="like_post_set"
+    )
+
+    def __str__(self):
+        return self.caption
+
+    class Meta:
+        ordering = ["-id"]
 
