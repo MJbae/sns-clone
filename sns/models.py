65c1532f353d09d072ffebc14b0b93e4971b4b30
MJbae <bmj5208@gmail.com>
2022-01-08 11:42:42 +0900

    feat: add tag model

--- sns/models.py
+++ sns/models.py
@@ -37,3 +37,9 @@ class Comment(TimestampedModel):
     class Meta:
         ordering = ["-id"]
 
+
+class Tag(TimestampedModel):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
