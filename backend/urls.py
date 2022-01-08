555d127884dfdd883c41dd124ac1e41b4ad557ff
MJbae <bmj5208@gmail.com>
2022-01-08 11:30:42 +0900

    chore: apply black in entire project

--- backend/urls.py
+++ backend/urls.py
@@ -3,14 +3,14 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('accounts/', include('accounts.urls')),
-    path('', include('sns.urls')),
+    path("admin/", admin.site.urls),
+    path("accounts/", include("accounts.urls")),
+    path("", include("sns.urls")),
 ]
 
 if settings.DEBUG:
     import debug_toolbar
 
     urlpatterns += [
-        path('__debug__/', include(debug_toolbar.urls)),
+        path("__debug__/", include(debug_toolbar.urls)),
     ]
