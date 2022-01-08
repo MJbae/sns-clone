47065c387ce089899c1ca24b423ffd40f97066f7
MJbae <bmj5208@gmail.com>
2022-01-08 11:45:11 +0900

    feat: add commentadmin, tagadmin

--- sns/admin.py
+++ sns/admin.py
@@ -11,3 +11,12 @@ class PostAdmin(admin.ModelAdmin):
     def photo_tag(self, post):
         return mark_safe(f"<img src={post.photo.url} style='width: 100px;' />")
 
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    pass
