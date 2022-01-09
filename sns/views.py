eecbde6b88972d7100cc91ef3559f8c66ada89bf
MJbae <bmj5208@gmail.com>
2022-01-09 12:16:58 +0900

    feat: add business logic of listing posts

--- sns/views.py
+++ sns/views.py
@@ -1,6 +1,9 @@
+from datetime import timedelta
+from django.utils import timezone
 from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import AllowAny
+from django.db.models import Q
 from .models import Post
 from .serializer import PostSerializer
 
@@ -8,4 +11,14 @@ from .serializer import PostSerializer
 class PostViewSet(ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    permission_classes = [AllowAny]  # FIXME: 인증 적용 필요
+
+    def get_queryset(self):
+        timesince = timezone.now() - timedelta(days=3)
+        qs = super().get_queryset()
+        qs = qs.filter(
+            Q(author=self.request.user)
+            | Q(author__in=self.request.user.following_set.all())
+        )
+        qs = qs.filter(created_at__gte=timesince)
+        return qs
+
