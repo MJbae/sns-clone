431c3611659dc4eb1d45f6fe7774052f737bdb20
MJbae <bmj5208@gmail.com>
2022-01-11 15:35:52 +0900

    feat: add comment viewset

--- sns/views.py
+++ sns/views.py
@@ -1,14 +1,14 @@
 from datetime import timedelta
 from django.utils import timezone
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from rest_framework import status
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.decorators import action
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from django.db.models import Q
-from .models import Post
-from .serializer import PostSerializer
+from .models import Post, Comment
+from .serializer import PostSerializer, CommentSerializer
 
 
 class PostViewSet(ModelViewSet):
@@ -49,3 +49,23 @@ class PostViewSet(ModelViewSet):
         post = self.get_object()
         post.like_user_set.remove(self.request.user)
         return Response(status.HTTP_204_NO_CONTENT)
+
+
+class CommentViewSet(ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+
+    def get_serializer_context(self):
+        context = super().get_serializer_context()
+        context["request"] = self.request
+        return context
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        qs = qs.filter(post__pk=self.kwargs["post_pk"])
+        return qs
+
+    def perform_create(self, serializer):
+        post = get_object_or_404(Post, pk=self.kwargs["post_pk"])
+        serializer.save(author=self.request.user, post=post)
+        return super().perform_create(serializer)
