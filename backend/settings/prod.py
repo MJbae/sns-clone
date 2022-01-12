93fbdea38b8899736cbc04426062cf421a30636e
MJbae <bmj5208@gmail.com>
2022-01-12 20:59:46 +0900

    chore: set env of azure storage with dotenv

--- backend/settings/prod.py
+++ backend/settings/prod.py
@@ -1,1 +1,11 @@
+import os
+from dotenv import load_dotenv
 from .common import *
+
+load_dotenv(verbose=True)
+
+STATICFILES_STORAGE = "backend.storages.StaticAzureStorage"
+DEFAULT_FILE_STORAGE = "backend.storages.MediaAzureStorage"
+
+AZURE_ACCOUNT_NAME = os.getenv("AZURE_ACCOUNT_NAME")
+AZURE_ACCOUNT_KEY = os.getenv("AZURE_ACCOUNT_KEY")
