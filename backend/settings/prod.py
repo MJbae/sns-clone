c3da5e9029a7b3840ddd44743bf9287df1f24a5f
MJbae <bmj5208@gmail.com>
2022-01-12 22:30:55 +0900

    chore: set production env of DEBUG, ALLOWED_HOSTS

--- backend/settings/prod.py
+++ backend/settings/prod.py
@@ -7,5 +7,8 @@ load_dotenv(verbose=True)
 STATICFILES_STORAGE = "backend.storages.StaticAzureStorage"
 DEFAULT_FILE_STORAGE = "backend.storages.MediaAzureStorage"
 
+DEBUG = os.environ.get("DEBUG") in ["1", "t", "true", "T", "True"]
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
+
 AZURE_ACCOUNT_NAME = os.getenv("AZURE_ACCOUNT_NAME")
 AZURE_ACCOUNT_KEY = os.getenv("AZURE_ACCOUNT_KEY")
