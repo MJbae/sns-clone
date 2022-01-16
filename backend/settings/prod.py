7edc44bf5b8aff97333fbb409b09aa4f8be75076
MJbae <bmj5208@gmail.com>
2022-01-16 19:45:05 +0900

    chore: remove dotenv

--- backend/settings/prod.py
+++ backend/settings/prod.py
@@ -1,25 +1,21 @@
-import os
-from dotenv import load_dotenv
 from .common import *
 
-load_dotenv(verbose=True)
+DEBUG = os.environ.get("DEBUG") in ["1", "t", "true", "T", "True"]
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
 
 STATICFILES_STORAGE = "backend.storages.StaticAzureStorage"
 DEFAULT_FILE_STORAGE = "backend.storages.MediaAzureStorage"
 
-DEBUG = os.environ.get("DEBUG") in ["1", "t", "true", "T", "True"]
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
-
-AZURE_ACCOUNT_NAME = os.getenv("AZURE_ACCOUNT_NAME")
-AZURE_ACCOUNT_KEY = os.getenv("AZURE_ACCOUNT_KEY")
+AZURE_ACCOUNT_NAME = os.environ["AZURE_ACCOUNT_NAME"]
+AZURE_ACCOUNT_KEY = os.environ["AZURE_ACCOUNT_KEY"]
 
 DATABASES = {
     "default": {
-        "ENGINE": os.getenv("DB_ENGINE", "django.db.backends.postgresql"),
-        "HOST": os.getenv("DB_HOST"),
-        "USER": os.getenv("DB_USER"),
-        "PASSWORD": os.getenv("DB_PASSWORD"),
-        "NAME": os.getenv("DB_NAME", "postgres"),
+        "ENGINE": os.environ.get("DB_ENGINE", "django.db.backends.postgresql"),
+        "HOST": os.environ["DB_HOST"],
+        "USER": os.environ["DB_USER"],
+        "PASSWORD": os.environ["DB_PASSWORD"],
+        "NAME": os.environ.get("DB_NAME", "postgres"),
     },
 }
 
